9cbda7eb3f0f6f7ebcd6e2fc5b19a7a5497a3163
waheedkhan65 <waheedkhan957759@gmail.com>
2026-07-27 17:52:24 +0500

    variables file code

---
+++ 02variables.php
@@ -0,0 +1,22 @@
+<?php
+// Concept: Variables
+// Variables are used to store data that can be reused in a script.
+
+$name = "Ayesha";
+$age = 21;
+$price = 19.99;
+
+// Simple example
+echo "Hello, " . $name . "!<br>";
+echo "You are " . $age . " years old.<br>";
+echo "Price: $" . $price . "<br>";
+
+// Real-world use example
+// A website may store a customer's name, age, and selected product price.
+$customerName = "Ali";
+$customerAge = 30;
+$cartTotal = 150.50;
+
+echo "Welcome back, " . $customerName . "!<br>";
+echo "Your cart total is $" . $cartTotal . "<br>";
+?>
