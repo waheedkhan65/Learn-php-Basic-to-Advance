a66a8342e068fcf819b577731790429a30756a6b
waheedkhan65 <waheedkhan957759@gmail.com>
2026-07-27 17:51:20 +0500

    echo-print statement file code

---
+++ 01echo-print.php
@@ -0,0 +1,17 @@
+<?php
+// Concept: echo and print
+// These statements are used to display output in PHP.
+
+// Simple example
+echo "Hello, welcome to PHP!<br>";
+print "This message is printed using print.<br>";
+
+// A variable can also be displayed
+$a = "PHP is easy to learn";
+echo $a . "<br>";
+
+// Real-world use example
+// A website may use echo to show a welcome message or product name.
+$productName = "Laptop";
+echo "Our featured product is: " . $productName . "<br>";
+?>
